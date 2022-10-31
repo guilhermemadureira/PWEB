340d8bf719b5041f0ec04b00f26e5685f0ddc1d7
Guilherme <64824790+guilhermemadureira@users.noreply.github.com>
2022-10-31 13:06:33 -0300

    Add files via upload

---
+++ Atividade10/Atividade10.js
@@ -0,0 +1,16 @@
+var aluno1 = {
+    RA: 10,
+    nome: "gui"
+};
+
+var aluno2 = {}
+aluno2.RA = 12;
+aluno2.nome = "denise";
+
+var aluno3 = new Object();
+aluno3.RA = 13;
+aluno3.nome= "Jean";
+
+alert(aluno1.RA + aluno1.nome);
+alert(aluno2.RA + aluno2.nome);
+alert(aluno3.RA + aluno3.nome);
